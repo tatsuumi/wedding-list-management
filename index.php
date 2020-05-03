209876236b04d792355f49acc4979bc6c6beb1cb
tatsuumi <tatsuumi227@gmail.com>
2020-05-03 20:54:45 +0900

    完成002

--- index.php
+++ index.php
@@ -1,46 +1,36 @@
 <?php
 
-        //postgresデータ追加
-        $conn = "host=ec2-34-195-169-25.compute-1.amazonaws.com dbname=d7v30lqsd1nu6g user=vupprickmnebwc password=8497d96c15036bf8ce3d851645d7b75c84bb42d3590660d78a07f201792c4063";
-        $link = pg_connect($conn);
-        $sql = 'SHOW TABLES';
-$stmt = $dbh->query($sql);
+        //pg_connect()に渡すパラメータの指定
+        $constr = "host=ec2-34-195-169-25.compute-1.amazonaws.com port=5432 dbname=d7v30lqsd1nu6g user=vupprickmnebwc password=8497d96c15036bf8ce3d851645d7b75c84bb42d3590660d78a07f201792c4063";
+         //DBに接続
+         $conn = pg_connect($constr);
+        //SQLの実行
+        $result = pg_query($conn, "SELECT * FROM people");
+        //データの取得
+        $arr = pg_fetch_all($result);
 
-while ($result = $stmt->fetch(PDO::FETCH_NUM)){
-    $table_names[] = $result[0];
-}
+        print "<table id=\"dblist\" summary=\"結婚式参加者データ一覧\">\n";
+        print "<caption>結婚式参加者データ一覧</caption>\n";
 
-$table_data = array();
-foreach ($table_names as $key => $val) {
-    $sql2 = "SELECT * FROM $val;";
-    $stmt2 = $dbh->query($sql2);
-    $table_data[$val] = array();
-    while ($result2 = $stmt2->fetch(PDO::FETCH_ASSOC)){
-        foreach ($result2 as $key2 => $val2) {
-            $table_data[$val][$key2] = $val2;
+        //テーブルヘッダとしてフィールド（カラム）名を出力
+        print "<tr>\n";
+        $flds = pg_num_fields($result);
+        for($i=0; $i<$flds; $i++){
+          $field = pg_field_name($result, $i);
+         printf("<th abbr=\"%s\">%s</th>\n", $field, $field);
         }
-    }
-}foreach ($table_data as $key => $val) {
-    echo "<h1>$key</h1>";
-    if (empty($val)) {
-        continue;
-    }
-    echo "<table border=1 style=border-collapse:collapse;>";
-    echo "<tr>";
-    foreach ($table_data[$key] as $key2 => $val2) {
-    echo "<th>";
-    echo $key2;
-    echo "</th>";
-    }
-    echo "</tr>";
-    echo "<tr>";
-    foreach ($table_data[$key] as $key2 => $val2) {
-    echo "<td>";
-    echo $val2;
-    echo "</td>";
-    }
-    echo "</tr>";
-    echo "</table>";
-}
-        $result_flag = pg_query($sql);
-        $close_flag = pg_close($link);
+        print "</tr>\n";
+
+        //データの出力
+        foreach($arr as $rows){
+        print "<tr>\n";
+        foreach($rows as $value){
+         printf("<td>%s</td>\n", $value);
+        }
+        print "</tr>\n";
+        }
+
+print "</table>\n";
+
+//DBとの接続を閉じる
+pg_close($conn);
