4647ed817a0d0e19bd6a7af1cc8a623656f0dd7d
tatsuumi <tatsuumi227@gmail.com>
2020-05-10 13:26:40 +0900

    db変更

--- delete_row.php
+++ delete_row.php
@@ -3,7 +3,7 @@
 $id = $_POST['id'];
 
 //pg_connect()に渡すパラメータの指定
-$constr = "host=ec2-34-195-169-25.compute-1.amazonaws.com port=5432 dbname=d7v30lqsd1nu6g user=vupprickmnebwc password=8497d96c15036bf8ce3d851645d7b75c84bb42d3590660d78a07f201792c4063";
+$constr =  "host=ec2-35-171-31-33.compute-1.amazonaws.com port=5432 dbname=d9canmf389vsq5 user=ceoecrklypftbt password=f68e1b403c34f2de22faabb1090e1d13005192acd1edaa6b22ce243120c0cc32";
 //DBに接続
 $conn = pg_connect($constr);
 //SQLの実行
